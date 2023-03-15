2842d8c6ff8979a23b03db1a7a32f1903e30d58c
mmandapati <monicalakshmi.mandapati@sjsu.edu>
2023-03-15 01:33:17 -0700

    Initial Product List page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import data from './data';
 
 function App() {
   return (
@@ -6,7 +7,23 @@ function App() {
       <header>
         <a href="/">EasyBuy</a>
       </header>
-      <main>Product Lists</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <p>{product.brand}</p>
+                <p> {product.name}</p>
+                <button className="btn btn-sm btn-primary">Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
